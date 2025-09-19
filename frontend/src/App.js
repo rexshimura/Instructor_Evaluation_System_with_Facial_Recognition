c6ca72138854e81148c4dd4360ce838632bdcde0
Rexshimura <mortpauljpm03@gmail.com>
2025-09-19 21:46:04 +0800

    adjusted route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,23 +2,34 @@ import React from "react";
  import { Routes, Route } from "react-router-dom";
 
 import Landing from "./pages/Landing";
-import StudentLogin from "./pages/StudentLogin";
-import ModeratorLogin from "./pages/ModeratorLogin";
-import AdminLogin from "./pages/AdminLogin";
-import InstructorFaceRec from "./pages/05-instructor/InstructorFaceRec";
 
-import Home from "./pages/03-student/Home";
+// LOGINS
+import StudentLogin from "./pages/00-auth/StudentLogin";
+import ModeratorLogin from "./pages/00-auth/ModeratorLogin";
+import AdminLogin from "./pages/00-auth/AdminLogin";
+
+// ADMIN ROUTES
+import AdminPanel from "./pages/01-administration/admin_panel";
+
+// MODERATOR ROUTES
 import ModeratorPanel from "./pages/02-moderator/moderator_panel";
-import ProtectedRoute from "./token/ProtectedRoute";
 import RegisterForm from "./pages/02-moderator/register_instructor/register_form";
 import FaceRecord from "./pages/02-moderator/register_instructor/register_face";
 import InstructorList from "./pages/02-moderator/instructor_list/instructor_list";
+
+// STUDENT ROUTES
+import Home from "./pages/03-student/Home";
 import StudentInstructorList from "./pages/03-student/Evaluate";
 import EvaluationForm from "./pages/03-student/EvaluationForm";
-import InstructorProfile from "./pages/05-instructor/InstructorProfile";
-import AdminPanel from "./pages/01-administration/admin_panel";
+
+// ERROR HANDLERS
+import ProtectedRoute from "./token/ProtectedRoute";
 import NotFound from "./pages/04-error/placeholder-notfound";
 import LoginBlock from "./pages/04-error/placeholder-loginblock";
+
+// INSTRUCTOR ROUTES
+import InstructorProfile from "./pages/05-instructor/InstructorProfile";
+import InstructorFaceRec from "./pages/05-instructor/InstructorFaceRec";
 
 
 function App() {
