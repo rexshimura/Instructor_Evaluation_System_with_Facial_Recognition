03888834eac9218451531a9cd998799b1eb29ab1
Rexshimura <mortpauljpm03@gmail.com>
2025-09-26 13:10:01 +0800

    New Data Set: class

---
+++ frontend/src/data/list-class.js
@@ -0,0 +1,37 @@
+const classList = [
+  // ====================== BSIT ======================
+  // Year 1
+  { cl_classID: 1051, cl_year: 1, st_section: "A", il_instructor_list: [1050001, 1050002, 1050003] },
+  { cl_classID: 1052, cl_year: 1, st_section: "B", il_instructor_list: [1050004, 1050005, 1050006] },
+
+  // Year 2
+  { cl_classID: 2051, cl_year: 2, st_section: "A", il_instructor_list: [1050007, 1050008, 1050009] },
+  { cl_classID: 2052, cl_year: 2, st_section: "B", il_instructor_list: [1050010, 1050011, 1050012] },
+
+  // Year 3
+  { cl_classID: 3051, cl_year: 3, st_section: "A", il_instructor_list: [1050009, 1050010, 1050006] },
+  { cl_classID: 3052, cl_year: 3, st_section: "B", il_instructor_list: [1050009, 1050010, 1050012] }, // John Paul's section
+
+  // Year 4
+  { cl_classID: 4051, cl_year: 4, st_section: "A", il_instructor_list: [1050011, 1050012, 1050013] },
+  { cl_classID: 4052, cl_year: 4, st_section: "B", il_instructor_list: [1050014, 1050015, 1050001] },
+
+  // ====================== BSIS ======================
+  // Year 1
+  { cl_classID: 1061, cl_year: 1, st_section: "A", il_instructor_list: [1060001, 1060002, 1060003] },
+  { cl_classID: 1062, cl_year: 1, st_section: "B", il_instructor_list: [1060004, 1060005, 1060006] },
+
+  // Year 2
+  { cl_classID: 2061, cl_year: 2, st_section: "A", il_instructor_list: [1060007, 1060008, 1060009] },
+  { cl_classID: 2062, cl_year: 2, st_section: "B", il_instructor_list: [1060010, 1060011, 1060012] },
+
+  // Year 3
+  { cl_classID: 3061, cl_year: 3, st_section: "A", il_instructor_list: [1060013, 1060014, 1060015] },
+  { cl_classID: 3062, cl_year: 3, st_section: "B", il_instructor_list: [1060001, 1060002, 1060003] },
+
+  // Year 4
+  { cl_classID: 4061, cl_year: 4, st_section: "A", il_instructor_list: [1060004, 1060005, 1060006] },
+  { cl_classID: 4062, cl_year: 4, st_section: "B", il_instructor_list: [1060007, 1060008, 1060009] },
+];
+
+export default classList;
