12d74c8800b2eb8b01dfdd3b73f56471d67cc8a0
Rexshimura <mortpauljpm03@gmail.com>
2025-09-26 13:09:41 +0800

    New Data Set: students

---
+++ frontend/src/data/list-students.js
@@ -0,0 +1,23 @@
+const studentData = [
+  { st_studID: "20210001", st_fname: "Alice", st_mname: "Marie", st_lname: "Johnson", st_suffix: "", st_dob: "2002-05-14", st_sex: "Female", st_course: "BSIT", st_year: 2, st_section: "A", st_semester: 1 },
+  { st_studID: "20210002", st_fname: "John Paul", st_mname: "Panggo", st_lname: "Mahilom", st_suffix: "", st_dob: "2005-06-24", st_sex: "Male", st_course: "BSIT", st_year: 3, st_section: "B", st_semester: 1 },
+  { st_studID: "20210003", st_fname: "Cassandra", st_mname: "Rose", st_lname: "Lee", st_suffix: "Ma.", st_dob: "2001-12-03", st_sex: "Female", st_course: "BSIT", st_year: 4, st_section: "A", st_semester: 1 },
+  { st_studID: "20210005", st_fname: "Sophia", st_mname: "Anne", st_lname: "Cruz", st_suffix: "", st_dob: "2004-09-12", st_sex: "Female", st_course: "BSIS", st_year: 4, st_section: "A", st_semester: 1 },
+  { st_studID: "1348317", st_fname: "Joehanes", st_mname: "Polestico", st_lname: "Lauglaug", st_suffix: "", st_dob: "2005-02-21", st_sex: "Male", st_course: "BSIT", st_year: 3, st_section: "A", st_semester: 1 },
+  { st_studID: "20210008", st_fname: "Rhea", st_mname: "Jane", st_lname: "Lim", st_suffix: "", st_dob: "2003-08-10", st_sex: "Female", st_course: "BSIT", st_year: 3, st_section: "B", st_semester: 1 },
+  { st_studID: "20210009", st_fname: "Mark", st_mname: "Anthony", st_lname: "Perez", st_suffix: "", st_dob: "2002-01-25", st_sex: "Male", st_course: "BSIT", st_year: 3, st_section: "A", st_semester: 1 }
+];
+
+// st_studID
+// st_fname
+// st_mname
+// st_lname
+// st_suffix
+// st_dob
+// st_sex
+// st_course
+// st_year
+// st_section
+// st_semester
+
+export default studentData;
