0f8f523870416595a02b2238adb450a1d05f7c58
Rexshimura <mortpauljpm03@gmail.com>
2025-09-29 22:59:14 +0800

    linear set

--- frontend/src/data/admin.js
+++ frontend/src/data/admin.js
@@ -1,13 +1,5 @@
 const admins = [
-  {
-    adm_ID: "A001",
-    adm_username: "yizaha",
-    adm_password: "adm123", // In a real app, this should be a hashed password
-    adm_fname: "John Paul",
-    adm_lname: "Mahilom",
-    adm_mname: "P.",
-    date_created: "2024-05-20",
-  },
+  { adm_ID: "8801", adm_username: "yizaha", adm_password: "adm123", adm_fname: "John Paul", adm_lname: "Mahilom", adm_mname: "P.", date_created: "2024-05-20"},
 ];
 
 export default admins;
--- frontend/src/data/moderators.js
+++ frontend/src/data/moderators.js
@@ -1,24 +1,7 @@
 const moderators = [
-  {
-    mod_ID: "M001",
-    mod_username: "rexshimura",
-    mod_password: "mod123", // In a real app, this should be a hashed password
-    mod_fname: "Rex",
-    mod_lname: "Zahard",
-    mod_mname: "L.",
-    date_created: "2024-05-20",
-    created_by: "ADMIN",
-  },
-  {
-    mod_ID: "M002",
-    mod_username: "jane.smith",
-    mod_password: "securepassword",
-    mod_fname: "Jane",
-    mod_lname: "Smith",
-    mod_mname: "B.",
-    date_created: "2024-05-21",
-    created_by: "ADMIN",
+  { mod_ID: "7701", mod_username: "rexshimura", mod_password: "mod123", mod_fname: "Rex", mod_lname: "Zahard", mod_mname: "L.", date_created: "2024-05-20", created_by: "ADMIN",},
+  { mod_ID: "7702", mod_username: "jane.smith", mod_password: "securepassword", mod_fname: "Jane", mod_lname: "Smith", mod_mname: "B.", date_created: "2024-05-21", created_by: "ADMIN",
   },
 ];
-
+// In a real app, these should be a hashed password, but for now it's there for demo.
 export default moderators;
