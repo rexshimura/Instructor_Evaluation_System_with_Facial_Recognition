cad9913eed40b7bc7dd5bdb03b294ecbe155aa9c
Rexshimura <mortpauljpm03@gmail.com>
2025-09-30 00:37:03 +0800

    rename

--- frontend/src/data/moderators.js
+++ frontend/src/data/moderators.js
@@ -1,6 +1,6 @@
 const moderators = [
-  { mod_ID: "7701", mod_username: "rexshimura", mod_password: "mod123", mod_fname: "Rex", mod_lname: "Zahard", mod_mname: "L.", date_created: "2024-05-20", created_by: "ADMIN",},
-  { mod_ID: "7702", mod_username: "jane.smith", mod_password: "securepassword", mod_fname: "Jane", mod_lname: "Smith", mod_mname: "B.", date_created: "2024-05-21", created_by: "ADMIN",
+  { mod_ID: "7701", mod_username: "rexshimura", mod_password: "mod123", mod_fname: "Rex", mod_lname: "Zahard", mod_mname: "L", date_created: "2024-05-20", created_by: "ADMIN",},
+  { mod_ID: "7702", mod_username: "modtest", mod_password: "1111", mod_fname: "Test", mod_lname: "Account", mod_mname: "T", date_created: "2024-05-21", created_by: "ADMIN",
   },
 ];
 // In a real app, these should be a hashed password, but for now it's there for demo.
