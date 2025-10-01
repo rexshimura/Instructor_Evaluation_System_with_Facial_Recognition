665d0665656f2b9272fb66d3a996dbb24360afca
Rexshimura <mortpauljpm03@gmail.com>
2025-10-01 17:25:06 +0800

    renamed

--- frontend/src/data/admin.js
+++ frontend/src/data/admin.js
@@ -1,5 +1,5 @@
 const admins = [
-  { adm_ID: "8801", adm_username: "yizaha", adm_password: "adm123", adm_fname: "John Paul", adm_lname: "Mahilom", adm_mname: "P.", date_created: "2024-05-20"},
+  { adm_ID: "8801", adm_username: "yizaha", adm_password: "adm123", adm_fname: "Rex", adm_lname: "Zahard", adm_mname: "X."},
 ];
 
 export default admins;
