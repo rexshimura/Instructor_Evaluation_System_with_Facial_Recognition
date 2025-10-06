ab9699025970dd0e78a5dbe12bdca412ddea040e
Yohhannn <joehanes.lauglaug@ctu.edu.ph>
2025-10-07 01:10:54 +0800

    Django trial

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Hello, world. You're at the polls page.")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
