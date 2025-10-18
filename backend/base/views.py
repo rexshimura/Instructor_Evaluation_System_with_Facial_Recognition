c2b752dfbf9d88606a478a99b50914c1ff06d9cd
Yohhannn <joehanes.lauglaug@ctu.edu.ph>
2025-10-19 04:28:36 +0800

    Views testing

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def home(request):
-    return HttpResponse("Hello, sdfgsdfsfsdworld. You'sdsdasdre at the polls page.")
+# def home(request):
+#     return HttpResponse("Hello, sdfgsdfsfsdworld. You'sdsdasdre at the polls page.")
