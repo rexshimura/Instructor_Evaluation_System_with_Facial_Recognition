e69dd853db6fe7cc6e7f6779be1cad45547e73cb
Yohhannn <joehanes.lauglaug@ctu.edu.ph>
2025-10-19 04:39:08 +0800

    Removed based files

--- backend/base/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# def home(request):
-#     return HttpResponse("Hello, sdfgsdfsfsdworld. You'sdsdasdre at the polls page.")
